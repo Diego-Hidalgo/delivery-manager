b199f43687c85b4b106f921cc7e2ce4f3fb08264
Brian Romero <briomero76@gmail.com>
2021-03-14 23:30:57 -0500

    add dishtype has been added

--- src/model/DeliveryManagerController.java
+++ src/model/DeliveryManagerController.java
@@ -7,12 +7,65 @@ public class DeliveryManagerController {
 	private List<User> user;
 	private List<Product> product;
 	private List<Employee> employee;
-	
+	private List<DishType> types;
 	public DeliveryManagerController(){
 		order = new ArrayList<Order>();
 		user = new ArrayList<User>();
 		product = new ArrayList<Product>();
 		employee = new ArrayList<Employee>();
+		types = new ArrayList<DishType>();
 	}//End DeliveryManagerController
 	
+	public boolean addProduct(String dishType){
+		boolean added = false;
+		if(findDishType(dishType) < 0){
+			if(types.isEmpty()){
+				types.add(new DishType(dishType));
+			}else {
+				DishType dt = new DishType(dishType);
+				int j = 0;
+				while(types.get(j).compareTo(dt) < 0){j++;}//End while
+				types.add(dt);
+			}//End else
+			added = true;
+		}//end if
+		return added;
+	}//End addProduct
+	
+	public int findDishType(String dishType){
+		int index = -1;
+		int start = 0;
+		int end = types.size() - 1;
+		while( start <= end) {
+			int half = (start - end)/2;
+			if(types.get(half).getName().compareTo(dishType) == 0){
+				index = half;
+			}else if(types.get(half).getName().compareTo(dishType) > 0){
+				end = half;
+			}else{
+				start = half;
+			}//End else
+		}//End while
+		return index;
+	}//End findDishType
+	public void changeDishType(String oldName, String newName){
+		int index = findDishType(oldName);
+		if(index >= 0 && findDishType(newName) < 0)
+			types.get(index).setName(newName);
+	}//End changeIngredient
+	public boolean removeIngredient(String name){
+		int index = findDishType(name);
+		boolean removed = false;
+		if(index >= 0)
+			if(!types.get(index).getLinked()){
+				types.remove(index);
+				removed = true;
+			}//End if
+		return removed;
+	}//End deleteIngredient
+	public void disableDishType(String name){
+		int index = findDishType(name);
+		if(index >= 0)
+			types.get(index).setEnable(false);
+	}//End disableIngredient
 }//End DeliveryManagerController
