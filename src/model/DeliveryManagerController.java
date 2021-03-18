a860054f5ccfc1acdf63387183a7cc3559da3bb0
Diego Hidalgo Lopez <d13g0fhl@gmail.com>
2021-03-18 10:10:32 -0500

    The addCustomer has been added

--- src/model/DeliveryManagerController.java
+++ src/model/DeliveryManagerController.java
@@ -1,18 +1,47 @@
 package model;
+import java.util.Comparator;
 import java.util.List;
 import java.util.ArrayList;
 
 public class DeliveryManagerController {
-	private List<Order> order;
-	private List<User> user;
-	private List<Product> product;
-	private List<Employee> employee;
+	private List<Order> orders;
+	private List<Customer> customers;
+	private List<User> users;
+	private List<Product> products;
+	private List<Employee> employees;
 	
 	public DeliveryManagerController(){
-		order = new ArrayList<Order>();
-		user = new ArrayList<User>();
-		product = new ArrayList<Product>();
-		employee = new ArrayList<Employee>();
+		orders = new ArrayList<Order>();
+		customers = new ArrayList<Customer>();
+		users = new ArrayList<User>();
+		products = new ArrayList<Product>();
+		employees = new ArrayList<Employee>();
 	}//End DeliveryManagerController
-	
+	public void addCustomer(String name, String lastName, String address, String nPhone, String remark) {
+		Customer newCustomer = new Customer(name, lastName, address, nPhone, remark);
+		if(customers.isEmpty()) {
+			customers.add(newCustomer);
+		} else {
+			Comparator<Customer> lastNameAndNameComparator = new LastNameAndNameComparator();
+			int i = 0;
+			while(i < customers.size() && lastNameAndNameComparator.compare(newCustomer, customers.get(i)) < 0){
+				i ++;
+			}
+			customers.add(i, newCustomer);
+		}
+	}//End addCustomer
+	public void addCustomer(String name, String lastName, String id,String address, String nPhone, String remark){
+		Customer newCustomer = new Customer(name, lastName, id, address, nPhone, remark);
+		if(customers.isEmpty()) {
+			customers.add(newCustomer);
+		} else {
+			Comparator<Customer> lastNameAndNameComparator = new LastNameAndNameComparator();
+			int i = 0;
+			while(i < customers.size() && lastNameAndNameComparator.compare(newCustomer, customers.get(i)) < 0){
+				i ++;
+			}
+			customers.add(i, newCustomer);
+		}
+	}//End addCustomer
+
 }//End DeliveryManagerController
---
+++ src/model/LastNameAndNameComparator.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.util.Comparator;
+
+public class LastNameAndNameComparator implements Comparator<Customer>{
+
+    @Override
+    public int compare(Customer c1, Customer c2){
+        if(c2.getLastName().compareTo(c1.getLastName()) != 0){
+            return c2.getLastName().compareTo(c1.getLastName());
+        } else{
+            return c2.getName().compareTo(c1.getName());
+        }
+    }
+
+}
